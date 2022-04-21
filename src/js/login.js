03cb60c5017b3da5ae31b2fbd886d0e0ac47d844
Teech300 <taher.chaouachi@sysdata.it>
2022-04-21 10:35:42 +0200

    login-tailwind in progress

--- src/js/login.js
+++ src/js/login.js
@@ -13,6 +13,39 @@ const usersList = [
   },
 ];
 
+function checkLogin(event) {
+  event.preventDefault();
+
+  let errorDiv = document.getElementById("error");
+  let h4 = document.createElement("h4");
+
+  let email = document.querySelector("#mail").value;
+  let password = document.querySelector("#password").value;
+
+  if (errorDiv.children[0]) {
+    errorDiv.removeChild(errorDiv.children[0]);
+  }
+
+  h4.classList.add("h2-error");
+  h4.innerText = "";
+  errorDiv.appendChild(h4);
+
+  if (validateEmail(email) === null) {
+    if (password === "") {
+      h4.innerText = "Email and Password are invalid";
+      errorEmailPassword();
+    } else {
+      h4.innerText = "Email is invalid";
+      changeTemplate("mail", "email-error", "email");
+    }
+  } else if (password === "") {
+    h4.innerText = "Password is invalid";
+    changeTemplate("password", "password-error", "password");
+  } else {
+    controlEmailAndPassword(email, password, h4);
+  }
+}
+
 function validateEmail(email) {
   return String(email)
     .toLowerCase()
@@ -21,61 +54,26 @@ function validateEmail(email) {
     );
 }
 
-function checkLogin(event) {
-  changeTemplate("password", "password", "password-error");
-  changeTemplate("mail", "email", "email-error");
-  document.getElementById("error").innerHTML = "<h2></h2>";
-
-  event.preventDefault();
-  let email = document.querySelector("#mail").value;
-  let password = document.querySelector("#password").value;
-
-  if (validateEmail(email) === null) {
-    if (password === "") {
-      changeTemplate("password", "password-error", "password");
-      changeTemplate("mail", "email-error", "email");
-      document.getElementById("error").innerHTML =
-        "<h2 class='h2-error'>Email or Password is invalid</h2>";
+function controlEmailAndPassword(matchMail, matchPassword, errorText) {
+  usersList.map((user) => {
+    if (!(user.mail === matchMail && user.password === matchPassword)) {
+      errorEmailPassword();
+      errorText.innerText = "Email and/or Password is invalid";
     } else {
-      changeTemplate("mail", "email-error", "email");
-      document.getElementById("error").innerHTML =
-        "<h2 class='h2-error'>Email is invalid</h2>";
+      window.location.replace("/src/pages/dashboard.html");
     }
-  } else if (password === "") {
-    changeTemplate("password", "password-error", "password");
-    document.getElementById("error").innerHTML =
-      "<h2 class='h2-error'> Password is invalid</h2>";
-  } else {
-    controlEmailAndPassword(email, password);
-  }
-
-}
-
-function controlEmailAndPassword(matchMail, matchPassword) {
-  filterMailPassword = usersList.filter(
-    (data) =>
-      data.mail.toLowerCase().includes(matchMail) &&
-      data.password.toLowerCase().includes(matchPassword)
-  );
-  console.log(filterMailPassword);
-  if (filterMailPassword.length == 1) {
-    filterMailPassword.map((x) => {
-      if (x.mail === matchMail && x.password === matchPassword) {
-        console.log("yes");
-        window.location.replace("/src/pages/dashboard.html");
-      } else {
-        console.log("no");
-      }
-    });
-  } else {
-    changeTemplate("password", "password-error", "password");
-    changeTemplate("mail", "email-error", "email");
-    document.getElementById("error").innerHTML =
-      "<h2 class='h2-error'>Email or Password is invalid</h2>";
-  }
+  });
 }
 
 function changeTemplate(id, newClass, oldClass) {
   document.getElementById(id).classList.add(newClass);
   document.getElementById(id).classList.remove(oldClass);
+}
+
+function errorEmailPassword() {
+  document.getElementById("mail").classList.add("email-error");
+  document.getElementById("mail").classList.remove("email");
+
+  document.getElementById("password").classList.add("password-error");
+  document.getElementById("password").classList.remove("password");
 }
